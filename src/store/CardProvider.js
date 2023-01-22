c28c1281d1684989e4bc9e58041b3894661537b3
samanfathnazarian <saman.nazarian17@gmail.com>
2023-01-22 04:14:42 -0600

    Implemented increasing quantity in cart when adding the same product

--- src/store/CardProvider.js
+++ src/store/CardProvider.js
@@ -6,11 +6,39 @@ const defaultCartState = {
   totalAmount: 0,
 };
 
+/*
+The item in cart state will have this object:
+  cartCtx.addItem({
+  id: props.id,
+  name: props.name,
+  amount: amount,
+  price: props.price,
+});
+*/
+
 // shouldn't be recreated when comonent is reevaluated
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item); //concat returns a new arr, updating state in an immutable way
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id); //find existing item
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    // if an existing item already exits then add a counter to the amount instead of adding a new one
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount, //add the amount which was added by current action to already added item
+      };
+
+      updatedItems = [...state.items]; //update arr immutably without editting original one
+      updatedItems[existingCartItemIndex] = updatedItem; //update that item
+    } else {
+      updatedItems = state.items.concat(action.item); //concat returns a new arr, updating state in an immutable way
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
