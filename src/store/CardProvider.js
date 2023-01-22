1ea74d94d26f991333e0beafe28a3579c1c2cc24
samanfathnazarian <saman.nazarian17@gmail.com>
2023-01-22 05:14:13 -0600

    Implemented ability to remove items from cart

--- src/store/CardProvider.js
+++ src/store/CardProvider.js
@@ -44,7 +44,27 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
-  return defaultCartState;
+  if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
+
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id); //remove the item obj from array entirly
+    } else {
+      // remove 1 quantity from amount
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 }; //update amount for existing item
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
 };
 
 const CartProvider = (props) => {
@@ -55,7 +75,7 @@ const CartProvider = (props) => {
   };
 
   const removeItemFromCartHandler = (id) => {
-    dispatchCartAction({ type: 'REMOVE', item: id });
+    dispatchCartAction({ type: 'REMOVE', id: id });
   };
 
   const cartContext = {
