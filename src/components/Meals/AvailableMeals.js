eeb8121637c5b75b623624fe2e88bbb34801d0f9
samanfathnazarian <saman.nazarian17@gmail.com>
2023-02-06 23:30:26 -0600

    Handled the Loading State

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,12 +6,14 @@ import classes from './AvailableMeals.module.css';
 // later fetch data from a database
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); //loading since the component was created
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch('https://react-http-4f104-default-rtdb.firebaseio.com/meals.json');
       const responseData = await response.json();
 
+      // console.log(responseData);
       const loadedMeals = [];
 
       for (const key in responseData) {
@@ -19,10 +21,16 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
     fetchMeals();
   }, []);
+
+  // doesn't get past the code below it since it has a return statement
+  if (isLoading) {
+    return <section className={classes.spinner}></section>;
+  }
 
   const mealsList = meals.map((meal) => <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />); //returns an arr of jsx components
 
