24ef8e331170cd2f1d51ee3aa91d0be9f47782bf
samanfathnazarian <saman.nazarian17@gmail.com>
2023-02-07 02:00:57 -0600

    Handled Errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,13 +7,18 @@ import classes from './AvailableMeals.module.css';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true); //loading since the component was created
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch('https://react-http-4f104-default-rtdb.firebaseio.com/meals.json');
       const responseData = await response.json();
 
-      // console.log(responseData);
+      // throwing error will cause promise that the async function returns to reject
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
       const loadedMeals = [];
 
       for (const key in responseData) {
@@ -24,12 +29,23 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   // doesn't get past the code below it since it has a return statement
   if (isLoading) {
     return <section className={classes.spinner}></section>;
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
   }
 
   const mealsList = meals.map((meal) => <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />); //returns an arr of jsx components
